37ba47dfc1f01abf322fa6c9ca566f92bd50cb69
germanmartinrodriguez-creator <germanmartinrodriguez@gmail.com>
2026-05-04 02:54:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'calendario-barcelona-repo-final-1';
+const ASSETS = ['./','./index.html','./manifest.json','./icono.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{})); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', event => { const req=event.request; if(req.method!=='GET') return; if(req.mode==='navigate'){ event.respondWith(fetch(req).then(res=>{caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',res.clone())).catch(()=>{});return res;}).catch(()=>caches.match('./index.html'))); return; } event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res=>{caches.open(CACHE_NAME).then(cache=>cache.put(req,res.clone())).catch(()=>{});return res;}).catch(()=>cached))); });
